ebaf2d4c902426dbbffa5ef650cde0dc5860a496
nhoem.ponleu <ponleunhoem@gmail.com>
2023-03-27 23:32:11 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,8 @@ public class Main {
             System.out.println("2.Insert Student");
             System.out.println("3.Update Student");
             System.out.println("4.Remove Student");
-            System.out.println("5.Exit");
+            System.out.println("5.Search Student");
+            System.out.println("6.Exit");
            // String strOption;
             System.out.println("Please Choose Option");
             String strOption;
@@ -32,7 +33,7 @@ public class Main {
 
             switch (option){
                case 1:
-                   System.out.println("===show====");
+                   System.out.println("=======show======");
                    for (int i=0;i<students.size();i++){
                        students.get(i).showInfo();
                        System.out.println("===========");
@@ -56,6 +57,7 @@ public class Main {
                     for (int i=0;i<students.size();i++){
                         if(idss==students.get(i).id){
                             System.out.println("============");
+                            String input= scc.nextLine();
                             System.out.println("Enter Your className:");
                             String className=scc.nextLine();
                             System.out.println("Enter Name:");
@@ -93,11 +95,67 @@ public class Main {
                    }
                    pressKey();
                    break;
-           }
+                case 5:
+                    int searchOption;
+                    System.out.println("************Search Worker***********");
+                    System.out.println("1. Search By ID ");
+                    System.out.println("2. Search By Name ");
+                    System.out.println("3.Search By Class Name");
+                    System.out.println("4. Exit....");
+
+                    System.out.print("Choose option 1 - 4 : ");
+                    searchOption = sc.nextInt();
+                    switch(searchOption){
+                        case 1 :
+                            int searchID;
+                            boolean isWorkerExist = false;
+                            System.out.println("*********** Search By ID *********** ");
+                            System.out.println("Enter ID to search : ");
+                            searchID = sc.nextInt();
+
+                            for(int i = 0; i < students.size(); i++){
+                                if(searchID== students.get(i).id){
+                                    // worker exist
+                                    isWorkerExist = true;
+                                    students.get(i).showInfo();
+                                }
+                            }
+                            if(!isWorkerExist){
+                                System.out.println("Worker with ID = "+searchID+" doesn't exist !!!");
+                            }
+                            pressKey();
+                            break;
+                        case 2 :
+                            System.out.println("*********** Search By Name *********** ");
+                            String searchName ;
+                            System.out.println("Enter name to search : ");
+                            searchName = sc.nextLine();
+                            boolean isWorkerExist1 = false;
+
+                            for(int i =0; i < students.size(); i++){
+                                if(searchName.equals(students.get(i).name)){
+                                    isWorkerExist1=true;
+                                    students.get(i).showInfo();
+                                    // user exist
+                                }
+
+                            }
+                            if(!isWorkerExist1){
+                                System.out.println("Worker with ID = "+searchName+" doesn't exist !!!");
+                            }
+
+
+                            sc.nextLine();
+                            break;
+
+                    }
+
+            }
 
 
 
-        }while (option!=5);
+
+        }while (option!=6);
 
 
     }
